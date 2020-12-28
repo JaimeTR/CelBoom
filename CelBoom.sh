2c63fd1acbbb2a8477c999b506ba681160db8a3b
Monkey-hk4 <69118685+Monkey-hk4@users.noreply.github.com>
2020-12-27 22:13:06 -0500

    Add files via upload

---
+++ CelBoom.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Created by: Monkey-hk4
+#
+# 
+
+function CelBoom {
+	sleep 0.5
+	clear
+echo -e ""
+echo -e ""
+echo -e "\e[1;34m           ▄▄· ▄▄▄ .▄▄▌  \e[0m""\e[31m▄▄▄▄·             • ▌ ▄ ·. \e[0m"
+echo -e "\e[1;34m          ▐█ ▌▪▀▄.▀·██•  \e[0m""\e[31m▐█ ▀█▪▪     ▪     ·██ ▐███▪\e[0m"
+echo -e "\e[1;34m          ██ ▄▄▐▀▀▪▄██▪  \e[0m""\e[31m▐█▀▀█▄ ▄█▀▄  ▄█▀▄ ▐█ ▌▐▌▐█·\e[0m"
+echo -e "\e[1;34m          ▐███▌▐█▄▄▌▐█▌▐▌\e[0m""\e[31m██▄▪▐█▐█▌.▐▌▐█▌.▐▌██ ██▌▐█▌\e[0m"
+echo -e "\e[1;34m          ·▀▀▀  ▀▀▀ .▀▀▀ \e[0m""\e[31m·▀▀▀▀  ▀█▄▀▪ ▀█▄▀▪▀▀  █▪▀▀▀\e[0m"
+sleep 0.5
+echo -e "\e[1;30m                      ▄    ▄▄▄▄▄▄▄    ▄     "
+echo -e "\e[1;30m                     ▀▀▄ ▄█████████▄ ▄▀▀    "
+echo -e "\e[1;30m                         ██─▀███▀─██        "
+echo -e "\e[1;30m                       ▄ ▀████▀████▀ ▄      "
+echo -e "\e[1;30m                     ▀█    ██▀█▀██    █▀    "
+echo ""                                                
+sleep 0.5
+echo -e "\e[32m                              2.1           "
+echo -e ""
+sleep 0.5
+echo -e "\e[33m                      code by : Monkey-hk4   "
+}
+CelBoom
+echo -e -n "\e[36m
+┌─┬┬┬┬──┬─┬┬┬─┐
+│││││││││┴┤┌┤┼│
+└┴─┴─┴┴┴┴─┴┘└─┘
+\e[35m════\e[32m>> \e[0m"
+read -r phone
+cd Impulse
+python impulse.py --method SMS --time 90 --threads 60 --target ${phone}
